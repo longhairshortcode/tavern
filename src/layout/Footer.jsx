b9a183997928a9c95cb459acd222e6989e64e964
Rona <longhairshortcode@gmail.com>
2024-03-11 19:38:14 -0700

    done with footer but not sure how complete the style.

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,9 +1,62 @@
+import style from "./Footer.module.css"
+import { Link } from "react-router-dom"
+import logo from "../assets/logo.png"
+import { IoLogoInstagram } from "react-icons/io";
+import { RiTwitterLine } from "react-icons/ri";
+import { RiFacebookBoxFill } from "react-icons/ri";
 
 function Footer() {
   return (
-    <div>
-      
-    </div>
+    <footer className={style.componentContainer}>
+      <div className={style.topInfoContainer}>
+        <div className={style.oneNameAndMission}>
+          <Link><img className={style.logo} src={logo} alt="company logo"></img></Link>
+          <p>A multi-cultural plant-based organic restaurant with elevated flavors. <span className={`${style['bold']} ${style['tagLine']}`}>Taste The World.</span></p>
+        </div>
+        <div className={style.twoExplore}>
+          <ul className={style.exploreContainer}>
+            <li className={`${style['bold']} ${style['explore']}`}>Explore</li>
+            <li>About</li>
+            <li>Menu</li>
+            <li>Catering</li>
+            <li>Location</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <div className={style.threeVisit}>
+          <address className={style.tavernAddress}>
+            <p className={`${style['bold']} ${style['visit']}`}>Visit</p>
+            <p className={style.actualAddress}>Tavern</p>
+            <p className={style.actualAddress}>123 Green Street</p>
+            <p className={style.actualAddress}>Los Angeles, CA</p>
+          </address>
+          <ul  className={style.contactContainer}>
+            <li className={`${style['bold']} ${style['contactUs']}`}>Contact Us</li>
+            <li><a className={style.email} href="mailto:contact@tavern.com">contact@tavern.com</a></li>
+            <li><a className={style.num} href="tel:818-555-1234">818-555-1234</a></li>
+          </ul>
+        </div>
+        <div className={style.fourFollow}>
+          <ul className={style.followContainer}>
+            <li className={`${style['bold']} ${style['follow']}`}>Follow</li>
+            <li><a><IoLogoInstagram /></a></li>
+            <li><a><RiTwitterLine /> </a></li>
+            <li><a><RiFacebookBoxFill /></a></li>
+          </ul>
+        </div>
+        <div className={style.fiveLegal}>
+          <ul className={style.legalContainer}>
+            <li className={`${style['bold']} ${style['legal']}`}>Legal</li>
+            <li><a className={style.terms}>Terms</a></li>
+            <li><a className={style.privacy}>Privacy</a></li>
+          </ul>
+        </div>
+        
+      </div>
+      <div className={style.botCopyrightContainer}>
+        <p>&copy; {new Date().getFullYear()} Tavern. All Rights Reserved</p>        
+      </div>
+    </footer>
   )
 }
 
