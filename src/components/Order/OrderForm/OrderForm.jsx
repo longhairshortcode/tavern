e19580e80d61bea148482edd3eee718bdee1fcb2
Rona <longhairshortcode@gmail.com>
2024-03-21 23:09:14 -0700

    got it to clear items selected by just setting showSelection to an empty array when handleReceiptClose is pressed.

--- src/components/Order/OrderForm/OrderForm.jsx
+++ src/components/Order/OrderForm/OrderForm.jsx
@@ -49,7 +49,8 @@ const [total, setTotal] = useState(0)
 //     }
 // };
 
-//Q - below should work if swap for above but it is not, why???
+//Q - below should work if swap for above but it is not, why?
+
 const handleSelection = (item) => {
     const updatedSelection = showSelection.map((selectedItem) =>
         selectedItem.id === item.id ? { ...selectedItem, quantity: selectedItem.quantity + 1 } : selectedItem
