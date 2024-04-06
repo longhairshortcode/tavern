70f35f0d70bbf468d43e6c2e463ede56d78dfe8a
Rona <longhairshortcode@gmail.com>
2024-04-06 00:12:01 -0700

    got cart to finally work correct;y, now styling scrollbar

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -38,7 +38,7 @@ useEffect(() => {
           
               <p className={style.itemDescription}>{item.itemDescription}</p>
               <button className={style.removeButton}>Remove</button>
-            {/* </div>        d */}
+            </div>        d
           
          )}
           {/* 3 */}
