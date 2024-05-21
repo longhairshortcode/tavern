8e2f738cb0213979dad3ad9c0048a5cee7ad76c0
Rona <longhairshortcode@gmail.com>
2024-05-21 02:21:48 -0700

    commit

--- src/pages/MenuOfRestaurant.jsx
+++ src/pages/MenuOfRestaurant.jsx
@@ -1,38 +1,31 @@
 import style from "./Styles/MenuOfRestaurant.module.css"
-import {NavLink, Outlet} from "react-router-dom"
-import menulogo from "../assets/menulogo.jpeg"
-
-// import { useState } from "react"
-
+import { NavLink, Outlet } from "react-router-dom"
+import tavern from "../assets/tavern.jpg"
 
 function MenuOfRestaurant() {
 
-  function stylenav({isActive}) {
+  function stylenav({ isActive }) {
     return {
-      backgroundColor: isActive ? "rgba(186, 153, 3, 0.557)": "orange",
+      backgroundColor: isActive ? "rgba(186, 153, 3, 0.557)" : "orange",
       color: isActive ? "rgb(145, 95, 2)" : "black",
     }
   }
-// const [showMenu, setShowMenu] = useState(true)
 
   return (
     <div className={style.componentContainer}>
       <div className={style.menuLeftAndRightContainer}>
         <div className={style.menuLeftContainer}>
           <p className={style.restaurantLeftTitle}>TAVERN RESTAURANT</p>
-          <img className={style.menuImage} src="/menulogo.jpeg" alt="restaurant logo"/>
+          <img className={style.menuImage} src={tavern} alt="restaurant logo" />
         </div>
         <div className={style.menuRightContainer}>
           <h2 className={style.menuRightTitle}>Food And Beverage Menu</h2>
           <div className={style.buttonsContainer}>
-            <NavLink style={stylenav} className={style.foodButton} to= "food-menu">Food</NavLink>
+            <NavLink style={stylenav} className={style.foodButton} to="food-menu">Food</NavLink>
             <NavLink style={stylenav} className={style.drinkButton} to="drink-menu">Drink</NavLink>
           </div>
-
-            <Outlet/>
-  
+          <Outlet />
         </div>
-        
       </div>
     </div>
   )
