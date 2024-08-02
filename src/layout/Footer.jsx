74fb6b1016bbf63f998dc3d6e71579ae73969397
longhairshortcode <116141725+longhairshortcode@users.noreply.github.com>
2024-08-01 23:12:38 -0700

    Update Footer.jsx

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,5 +1,5 @@
 
-sssssssssssimport style from "./Footer.module.css"
+ssssssssssssimport style from "./Footer.module.css"
 import { Link } from "react-router-dom"
 import logo from "../assets/logo.png"
 import { IoLogoInstagram } from "react-icons/io";
