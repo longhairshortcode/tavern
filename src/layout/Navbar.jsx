4e0798be7251765ca32a413dcaa2eaeb7305e47e
longhairshortcode <116141725+longhairshortcode@users.noreply.github.com>
2024-09-03 23:42:20 -0700

    Update Navbar.jsx

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,4 +1,4 @@
-SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSssimport style from './Navbar.module.css'
+SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSssimport style from './Navbar.module.css'
 import { NavLink, useNavigate } from "react-router-dom"
 import { useState } from "react"
 import StartOrder from '../common/StartOrder'
